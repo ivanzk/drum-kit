221bc91df9abca4b54ddefd5329ced9b2e20a8b5
Ivan <28441880+ivanzk@users.noreply.github.com>
2018-01-13 14:40:05 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-console.log('Hi');
+window.addEventListener('keydown', (e) => {
+	// get the keyCode, 'Y' and 'Z' keys play the same sample
+	let keycode = (e.keyCode == 90) ? 89 : e.keyCode,
+			key = document.querySelector(`.drum-element[data-key="${keycode}"]`);
+	
+	if (!key) return;
+	
+	// check if key was released
+	if (!key.classList.contains('hold')) play(e);
+	
+	key.classList.add('hold');
+});
 
-window.addEventListener('keydown', play);
+
+window.addEventListener('keyup', (e) => {
+	let keycode = (e.keyCode == 90) ? 89 : e.keyCode,
+			key = document.querySelector(`.drum-element[data-key="${keycode}"]`);
+	
+	if (!key) return;
+	
+	key.classList.remove('hold');
+});
+
 
 const keys = document.querySelectorAll('.drum-element');
 keys.forEach(key => {
@@ -10,9 +30,10 @@ keys.forEach(key => {
 
 
 function play(e) {
-	let keycode = e.keyCode;
+	// 'Y' and 'Z' key control the same sample
+	let keycode = (e.keyCode == 90) ? 89 : e.keyCode;
 	
-	if (e.type === 'mousedown') {
+	if (e.type == 'mousedown') {
 		keycode = e.currentTarget.attributes['data-key'].value;
 	}
 	
